6686f6436aafc98ddec85fb0c9c594447d157a24
Mushtari <geekcoder98@gmail.com>
2019-11-06 16:09:28 +0530

    Main Commit

--- HibernateExmpl/src/main/java/com/LTI/HibExamples/HibernateExmpl/App.java
+++ HibernateExmpl/src/main/java/com/LTI/HibExamples/HibernateExmpl/App.java
@@ -1,13 +1,24 @@
 package com.LTI.HibExamples.HibernateExmpl;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        Configuration c=new Configuration().configure();
+        StandardServiceRegistryBuilder builder=new StandardServiceRegistryBuilder().applySettings(c.getProperties());
+        SessionFactory sf=c.buildSessionFactory(builder.build());
+        Session session=sf.openSession();
+        Employ emp=new Employ();
+        emp.setEmpName("MUSHTARI");
+        emp.setBranch("JAVA");
+        session.beginTransaction();
+        session.save(emp);
+        session.getTransaction().commit();
     }
 }
